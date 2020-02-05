658e8e8b51250f4dc7dd152fccfda3bf163d7326
lisx10 <lisx10@asiainfo.com>
2020-02-05 11:01:30 +0800

    ui组件设计

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import styles from './index.css';
-import { Button } from 'antd';
+import { Button,Form } from 'antd';
+import {connect} from 'dva';
+import withRouter from 'umi/withRouter';
 import welcomeSvg from '@/assets/welcome.svg';
+import styles from './index.css';
 
-export default function() {
+const LoginComponment: React.FC = props => {
+  const {currentUser} = props;
+
+  if(!currentUser) return (
+    <div className={styles.container}>
+      <img src={welcomeSvg} className={styles.welcomeBg} alt="logo" />
+      <div className={styles.welcomeTitle}>歡迎</div>
+      <div className={styles.welcomeNote}>問靈/縛靈/祭靈/喚靈/攝靈</div>
+    </div>
+  );
+
   return (
     <div className={styles.container}>
       <img src={welcomeSvg} className={styles.welcomeBg} alt="logo" />
@@ -11,4 +23,8 @@ export default function() {
       <div className={styles.welcomeNote}>問靈/縛靈/祭靈/喚靈/攝靈</div>
     </div>
   );
-}
+};
+
+export default withRouter(connect(({ user })=>({
+  currentUser: user.currentUser,
+}))(Form.create({})(LoginComponment)));
